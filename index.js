7919d22fd3cb2f39e92cd819a299e48edcc1d40c
iHype <42493144+iHype@users.noreply.github.com>
2018-08-21 18:32:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ bot.commands = new Discord.Collection();
 bot.on("ready", async ready => {console.log("Bot ready");})
 bot.on("message", async message => {
   if(message.author.bot) return;
-  message.channel.send(message.channel.guild.id);
+  message.channel.send(message.guild.id);
   
 //  if(message.guild.id !== "282275654760660993" || message.guild.id !== "480854088876294144") return message.channel.send("You need to join FergFam or the support server in order to use this bot");
   
