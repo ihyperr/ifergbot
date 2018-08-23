20b4364739bb56ccc973ed6f232a8bd6f1994510
iHype <42493144+iHype@users.noreply.github.com>
2018-08-23 14:45:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -148,14 +148,7 @@ var ABC = {
  var binaryToText = ABC.toAscii(args.join(" "));
         message.channel.send(message.author + ": that thranslated to normal text is:");
         message.channel.send(binaryToText); }
-   /*
-  
-    if (cmd === "<@481524871038369803>" && commandbans.includes(message.author)) {
-  return message.author.sendMessage(message.author + ": you have been banned from using commands of this bot\nTo gain access back please DM <@430447525800181762>, <@299495028756054016>, <@453970692266786816> or any of the Mods/Admins of Ferg.") 
-  
-  
-  } if (cmd == "<@481524871038369803>" && !commandbans.includes(message.author)  {return message.channel.send(message.author + " no u")}
-  */
+
   /*
   
   if(cmd == `${prefix}ban` && message.author == "<@430447525800181762>") {
@@ -179,10 +172,7 @@ if(cmd == `${prefix}translate`) {
         console.error(err);
     });
 }
- if(messageArray.includes("<@478957124542529556>")) {
-     message.channel.send(message.author + " no u");
- }
-*/
+
  if(cmd === `${prefix}credits`) {
      let botembed = new Discord.RichEmbed()
      .setTitle("Credits")
