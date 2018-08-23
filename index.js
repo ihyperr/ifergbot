89357c7d114470d48aeff6ef9839e1dd7162d16c
iHype <42493144+iHype@users.noreply.github.com>
2018-08-23 02:40:34 +0000

    Update index.js

--- index.js
+++ index.js
@@ -156,14 +156,14 @@ var ABC = {
   */
   
   
-  /*if(cmd == `${prefix}ban` && message.author == "<@430447525800181762>") {
+  if(cmd == `${prefix}ban` && message.author == "<@430447525800181762>") {
     try {
     let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     console.log(bUser);
     bUser.ban();
     return;
     } catch(err){return console.error(err);}
-  }*/
+  }
 
 /* if(cmd == `${prefix}setreportchannel`) {
     if(!message.member.hasPermission("VIEW_AUDIT_LOG")) return message.channel.send(message.author + ": You can't do that! You are missing the permission:\nVIEW_AUDIT_LOG");
