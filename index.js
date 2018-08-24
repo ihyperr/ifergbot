84676d9a7c2551af3048cc64723cf4dd3a314283
iHype <42493144+iHype@users.noreply.github.com>
2018-08-24 19:06:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -113,8 +113,8 @@ translate(translateArg + "", {to: tragetLanguage + ""}).then(res => {
   
   if(cmd === `${prefix}nick` && message.author.id == "430447525800181762") {
     let userNick = message.mentions.members.first();
-    let nickName = args[1];
-    userNick.setNickname(nickName)
+
+    userNick.setNickname(translateArg)
   }
   if(cmd === `${prefix}texttobinary`) {
 var ABC = {
